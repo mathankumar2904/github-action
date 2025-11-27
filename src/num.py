a2c1080054fd188143b3ba2740a0e5fbe281162a
mathankumar2904 <mathankumar29042004@gmail.com>
2025-11-27 17:48:11 +0530

    delete the num.py

--- src/num.py
+++
@@ -1,8 +0,0 @@
-def evn(s):
-    if s%2==0:
-        return 1
-    else:
-        return 0
-def test_evn():
-    assert(2)==1
-    assert(1)==0
--- test/test_num.py
+++
@@ -1,7 +0,0 @@
-from num import evn
-
-def test_evn():
-    assert evn(2) == 1    # 2 is even → 1
-    assert evn(4) == 1    # even number
-    assert evn(1) == 0    # odd number → 0
-    assert evn(7) == 0    # odd number
